c30b8c8b3cb949497d3fa850a1ad6d69bfee389e
Xinqian liu <crystalynliu@163.com>
2016-08-05 15:59:50 +0800

    complete Diamond and modify the function structure

--- diamond-exercises/src/com/company/Main.java
+++ diamond-exercises/src/com/company/Main.java
@@ -5,35 +5,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.print("please input a number:");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         printTriangle(number);
+        printDiamond(number);
     }
 
-    public static void printTriangle(int number){
+    public static void printTriangle(int number) {
         System.out.println("=======Isosceles Triangle=======");
-        int length = number*2 - 1;
-        for(int i = 1; i <= number; i++){
-            int starNum = i*2 - 1;
-            int spaceNum = (length - starNum)/2;
-            circlePrintSpace(spaceNum);
-            circlePrintStar(starNum);
-            circlePrintSpace(spaceNum);
-            System.out.println();
+        int length = number * 2 - 1;
+        for (int i = 1; i <= number; i++) {
+            PrintStarLine(length, i);
         }
     }
 
-    public static void circlePrintSpace(int number){
-        for(int i = 0; i < number; i++){
+    public static void printDiamond(int number) {
+        System.out.println("=======Diamond=======");
+        int length = number * 2 - 1;
+        for (int i = 1; i <= number; i++) {
+            PrintStarLine(length, i);
+        }
+        for (int i = number - 1; i > 0; i--){
+            PrintStarLine(length, i);
+        }
+    }
+
+    public static void PrintStarLine(int length, int index) {
+        int starNum = index * 2 - 1;
+        int spaceNum = (length - starNum) / 2;
+        for (int j = 0; j < spaceNum; j++) {
             System.out.print(" ");
         }
-    }
-
-    public static void circlePrintStar(int number){
-        for(int i = 0; i < number; i++){
+        for (int j = 0; j < starNum; j++) {
             System.out.print("*");
         }
+        for (int j = 0; j < spaceNum; j++) {
+            System.out.print(" ");
+        }
+        System.out.println();
     }
 }
